c2b8b20f3cdc594367ae94404622fc7fbe09281f
sec-edu-ribeirao <sec.edu.ribeirao@hotmail.com>
2021-12-07 09:19:04 -0300

    Arrumando ambiente

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiURLBase: 'https://api-alunos-test.herokuapp.com',
+  apiURLBase: 'https://alunos-api-rib.herokuapp.com',
   clientID: 'my-angular-app',
   clientSecret: 'my-pass-app',
   getUrlToken: '/oauth/token',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiURLBase: 'https://api-alunos-test.herokuapp.com',
+  apiURLBase: 'https://alunos-api-rib.herokuapp.com',
   clientID: 'my-angular-app',
   clientSecret: 'my-pass-app',
   getUrlToken: '/oauth/token',
